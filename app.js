4364adf636fa3fe1710110046d09cf1246633e3c
belgacem-dahmen <belgacem.dahmen@gmail.com>
2023-12-05 18:42:37 +0100

    adding width and height to settings

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const vm = Vue.createApp({
     data() {
       return {
+        height :25,
+        width : 50,
         perspective : 0,
         rotateX : 0, 
         rotateY : 0, 
-        rotateZ : 0        
+        rotateZ : 0      
+      }
+    },
+    computed : {
+      box () {
+        return { 
+          height : `${this.height}px`,
+          width : `${this.width}px`,
+          transform : `
+          perspective(${this.perspective}px)
+          rotateX(${this.rotateX}deg)
+          rotateY(${this.rotateY}deg)
+          rotateZ(${this.rotateZ}deg)
+          `
+        }
       }
     },
     methods : {
         reset () {
-            this.perspective = 0;
+            this.perspective = 100;
             this.rotateX = 0;
             this.rotateY = 0; 
             this.rotateZ = 0;
 
         },
         copy () {
-            console.log(Navigator.clipBoard)
+            
         }
     }
   }).mount('#app')
