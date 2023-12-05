28c68723b9a36cd36162d16a96846d847e5b9f44
belgacem-dahmen <belgacem.dahmen@gmail.com>
2023-12-05 18:10:44 +0100

    playground exercice

---
+++ app.js
@@ -0,0 +1,22 @@
+const vm = Vue.createApp({
+    data() {
+      return {
+        perspective : 0,
+        rotateX : 0, 
+        rotateY : 0, 
+        rotateZ : 0        
+      }
+    },
+    methods : {
+        reset () {
+            this.perspective = 0;
+            this.rotateX = 0;
+            this.rotateY = 0; 
+            this.rotateZ = 0;
+
+        },
+        copy () {
+            console.log(Navigator.clipBoard)
+        }
+    }
+  }).mount('#app')
